c896b46e3307c6f865089e4395770492b78298dd
EVV1E <fabillo552@gmail.com>
2026-07-19 01:18:33 +0200

    Change ImagePatch coordinates to unsigned short

--- src/main/java/dev/evvie/waylandcraft/datasync/ImagePatch.java
+++ src/main/java/dev/evvie/waylandcraft/datasync/ImagePatch.java
@@ -13,10 +13,10 @@ public record ImagePatch(short format, int x, int y, int width, int height, byte
 	
 	public static final StreamCodec<ByteBuf, ImagePatch> STREAM_CODEC = StreamCodec.composite(
 			WaylandCraftUtils.UNSIGNED_BYTE, ImagePatch::format,
-			ByteBufCodecs.INT, ImagePatch::x,
-			ByteBufCodecs.INT, ImagePatch::y,
-			ByteBufCodecs.INT, ImagePatch::width,
-			ByteBufCodecs.INT, ImagePatch::height,
+			ByteBufCodecs.UNSIGNED_SHORT, ImagePatch::x,
+			ByteBufCodecs.UNSIGNED_SHORT, ImagePatch::y,
+			ByteBufCodecs.UNSIGNED_SHORT, ImagePatch::width,
+			ByteBufCodecs.UNSIGNED_SHORT, ImagePatch::height,
 			ByteBufCodecs.BYTE_ARRAY, ImagePatch::data,
 			ImagePatch::new
 	);
